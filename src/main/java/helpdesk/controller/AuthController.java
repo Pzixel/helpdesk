6ecb924c69d783e491b025bbc282a097d25c1e5b
barteromiew <barteromiew@gmail.com>
2023-06-27 16:59:18 +0300

    can create cookie

--- src/main/java/helpdesk/controller/AuthController.java
+++ src/main/java/helpdesk/controller/AuthController.java
@@ -1,16 +1,26 @@
 package helpdesk.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+// import cookie
 
 @RestController
 @EnableAutoConfiguration
 public class AuthController {
     @RequestMapping("/login")
-    String login() {
-        return "Hello World!";
+    String login(HttpServletResponse response) {
+        Cookie jwtTokenCookie = new Cookie("user-id", "c2FtLnNtaXRoQGV4YW1wbGUuY29t");
+        jwtTokenCookie.setMaxAge(86400);
+        jwtTokenCookie.setSecure(true);
+        jwtTokenCookie.setHttpOnly(true);
+
+        response.addCookie(jwtTokenCookie);
+
+        return "Ok";
     }
 
 }
