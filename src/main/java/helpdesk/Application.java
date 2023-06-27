e3b40ca49787b5aa56e2144c350f5b757ad9964e
barteromiew <barteromiew@gmail.com>
2023-06-27 16:53:48 +0300

    two controllers

--- src/main/java/helpdesk/Application.java
+++ src/main/java/helpdesk/Application.java
@@ -1,21 +1,11 @@
 package helpdesk;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@RestController
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
-
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
